f4c37a9d53ffd8f9086ca1739a4627c4e275f90f
Cham Ngoc Ng <nguyenngocchamhp@icloud.com>
2026-05-19 15:25:08 +0700

    cham

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
+import React from 'react';
+// Dòng import quan trọng nhất để sửa lỗi "not defined" của bạn:
+import { Gallery, Profile, Profile2 } from './components/Gallery';
+import Home from './components/Home'; // Nếu bạn có dùng cả Home component
 
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className="App" style={{ padding: '20px', color: 'white', backgroundColor: '#121212', minHeight: '100vh' }}>
+      <h1>This is Home components</h1>
+      
+      {/* Gọi các component ra sử dụng */}
+      <Profile />
+      <hr style={{ margin: '20px 0', opacity: 0.2 }} />
+      
+      <Profile2 />
+      <hr style={{ margin: '20px 0', opacity: 0.2 }} />
+      
+      <Gallery />
+    </div>
   );
 }
 
